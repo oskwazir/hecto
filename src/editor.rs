0ae90fe0d04582908db42e1797031d68c402be71
omer <omer@fastmail.com>
2026-08-24 16:37:44 -0400

    Use crossterm

--- src/editor.rs
+++ src/editor.rs
@@ -1,34 +1,31 @@
-use std::io::{self, Read};
+use crossterm::event::{Event::Key, KeyCode::Char, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
-pub struct Editor {
-
-}
+pub struct Editor {}
 
 impl Editor {
-
     pub fn default() -> Self {
-        Editor{}
+        Editor {}
     }
-
     pub fn run(&self) {
         enable_raw_mode().unwrap();
 
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03}", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?}\r", event);
+                    match (event.code) {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
                     }
+                }
 
-                    if c == 'q'  {
-                        break;
-                    }
-                 }
                 Err(err) => println!("Error: {}", err),
+                _ => (),
             }
         }
 

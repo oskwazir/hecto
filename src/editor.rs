5b3287e8695750bd615353f0ba011c7b33e94c4e
omer <omer@fastmail.com>
2026-08-26 12:12:40 -0400

    fixup

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{Event::Key, KeyCode::Char, read, KeyEvent, KeyModifiers};
+use crossterm::event::{Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 pub struct Editor {
@@ -18,16 +18,24 @@ impl Editor {
     fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
         loop {
-           if let Key(KeyEvent { code, modifiers, kind, state }) = read()? {
-               println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r");
+            if let Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                println!(
+                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
+                );
 
-               match code {
-                   Char('q') if modifiers == KeyModifiers::CONTROL => {
-                       self.should_quit = true;
-                   }
-                   _ => (),
-               }
-           }
+                match code {
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
+                    }
+                    _ => (),
+                }
+            }
 
             if self.should_quit {
                 break;

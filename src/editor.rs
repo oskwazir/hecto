c2341c9200773c6c65a14997346260dadaa888bb
omer <omer@fastmail.com>
2026-09-10 14:29:00 -0400

    Update to match the code review example

--- src/editor.rs
+++ src/editor.rs
@@ -1,66 +1,33 @@
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
-use crossterm::{execute, queue};
-use std::io::{Error, Write, stdout};
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor {
     should_quit: bool,
-    height: u16,
 }
 
 impl Editor {
-    pub fn default() -> Self {
-        Editor {
-            should_quit: false,
-            height: 0,
-        }
+    pub const fn default() -> Self {
+        Self { should_quit: false }
     }
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
-        self.height = crossterm::terminal::size().unwrap().1;
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn draw_rows(&self) -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        for number in 0..self.height {
-            queue!(stdout, crossterm::cursor::MoveTo(0, number))?;
-            print!("~");
-        }
-        stdout.flush()?;
-        Ok(())
-    }
-
-    fn clear_screen() -> Result<(), Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
-    }
-
     fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        self.draw_rows()?;
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(())
     }
-
     fn evaluate_event(&mut self, event: &Event) {
         if let Key(KeyEvent {
             code, modifiers, ..
@@ -74,11 +41,23 @@ impl Editor {
             }
         }
     }
-
-    fn refresh_screen(&mut self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             print!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
+        }
+        Ok(())
+    }
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let height = Terminal::size()?.1;
+        for current_row in 0..height {
+            print!("~");
+            if current_row + 1 < height {
+                print!("\r\n");
+            }
         }
         Ok(())
     }
